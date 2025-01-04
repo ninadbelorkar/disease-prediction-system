ccd3a03e1aa7d58a439c31d2c9b7b92f57c28b05
Ninad Belorkar <ninad.belorkar23@vit.edu>
2025-01-04 15:11:22 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,63 @@
+import os
+import pickle
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+diabetes_model = pickle.load(open(f'{working_dir}/diabetes_svm_model.sav', 'rb'))
+heart_disease_model = pickle.load(open(f'{working_dir}/heart_disease_model1.sav', 'rb'))
+parkinsons_model = pickle.load(open(f'{working_dir}/parkinsons_model.sav', 'rb'))
+
+# Home Page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Diabetes Prediction
+@app.route('/predict_diabetes', methods=['POST'])
+def predict_diabetes():
+    try:
+        data = [float(request.form[key]) for key in [
+            'Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin',
+            'BMI', 'DiabetesPedigreeFunction', 'Age']]
+        
+        prediction = diabetes_model.predict([data])[0]
+        result = 'The person is diabetic' if prediction == 1 else 'The person is not diabetic'
+        return jsonify({'result': result})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+# Heart Disease Prediction
+@app.route('/predict_heart_disease', methods=['POST'])
+def predict_heart_disease():
+    try:
+        data = [float(request.form[key]) for key in [
+            'age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg',
+            'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal']]
+        
+        prediction = heart_disease_model.predict([data])[0]
+        result = 'The person has heart disease' if prediction == 1 else 'The person does not have heart disease'
+        return jsonify({'result': result})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+# Parkinson's Prediction
+@app.route('/predict_parkinsons', methods=['POST'])
+def predict_parkinsons():
+    try:
+        data = [float(request.form[key]) for key in [
+            'fo', 'fhi', 'flo', 'Jitter_percent', 'Jitter_Abs',
+            'RAP', 'PPQ', 'DDP', 'Shimmer', 'Shimmer_dB',
+            'APQ3', 'APQ5', 'APQ', 'DDA', 'NHR',
+            'HNR', 'RPDE', 'DFA', 'spread1', 'spread2', 'D2', 'PPE']]
+        
+        prediction = parkinsons_model.predict([data])[0]
+        result = "The person has Parkinson's disease" if prediction == 1 else "The person does not have Parkinson's disease"
+        return jsonify({'result': result})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
